a8e330a08c513f04cfc19916d680b72e9359f52f
benhungerford <bmhungerford@gmail.com>
2020-10-06 16:30:00 -0400

    still not fully sure how to get it to work correctly

---
+++ improved_computer_guessing.py
@@ -0,0 +1,32 @@
+def computer_guess(number):
+    lower = 1
+    higher = 100
+    guess = 50
+    guesses = 0
+    while guesses < 6:
+        guess = (lower+higher)//2
+        print("The computer guesses", guess)
+        guesses += 1
+        if guess > number:
+            higher = guess
+        elif guess < number:
+            lower = guess + 1
+        elif guess == number:
+            break
+            
+        if guesses > 5:
+            print('The computer did not guess the number.')
+        elif guess == number:
+            print('The computer guessed', guess, 'correctly!')
+
+
+
+def main():
+    number = int(input("Enter a number: "))
+    if number < 1 or number > 100:
+        print("Must be in range [1, 100]")
+    else:
+        computer_guess(number)
+
+if __name__ == '__main__':
+    main()
